d54e831fe3a99907fdef95465fb1fbb5f378a696
Dekoder-py <dekod3r.py@gmail.com>
2025-10-20 09:00:17 +1300

    add beggining of soundbord

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,22 @@
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
+import { useAudioPlayer } from "expo-audio";
+
+const vineBoomAudio = require("./assets/boom.mp3");
+const taskmasterThemeAudio = require("./assets/tm_theme.mp3");
 
 export default function Index() {
+  const vineBoomPlayer = useAudioPlayer(vineBoomAudio);
+  function vineBoom() {
+    vineBoomPlayer.seekTo(0);
+    vineBoomPlayer.play();
+  }
+
+  const taskmasterThemePlayer = useAudioPlayer(taskmasterThemeAudio);
+  function taskmasterTheme() {
+    taskmasterThemePlayer.seekTo(0);
+    taskmasterThemePlayer.play();
+  }
+
   return (
     <View
       style={{
@@ -9,7 +25,12 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Pressable onPress={vineBoom}>
+        <Text>Vine Boom</Text>
+      </Pressable>
+      <Pressable onPress={taskmasterTheme}>
+        <Text>Taskmaster Theme</Text>
+      </Pressable>
     </View>
   );
 }
