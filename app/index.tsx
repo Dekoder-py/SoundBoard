d835f71273819250d265b6b077f54a977bef19bf
Dekoder-py <dekod3r.py@gmail.com>
2025-10-26 17:36:56 +1300

    add more sfx

--- app/index.tsx
+++ app/index.tsx
@@ -4,15 +4,21 @@ import { AudioPlayer, useAudioPlayer } from "expo-audio";
 const sounds = {
   vineBoom: require("./assets/boom.mp3"),
   taskmasterTheme: require("./assets/tm_theme.mp3"),
+  foxIntro: require("./assets/foxTheme.mp3"),
+  shaw: require("./assets/shaw.mp3"),
 };
 
 export default function Index() {
   const vineBoomPlayer = useAudioPlayer(sounds.vineBoom);
   const taskmasterThemePlayer = useAudioPlayer(sounds.taskmasterTheme);
+  const foxIntroPlayer = useAudioPlayer(sounds.foxIntro);
+  const shawPlayer = useAudioPlayer(sounds.shaw);
 
   const players: Record<string, AudioPlayer> = {
     vineBoom: vineBoomPlayer,
     taskmasterTheme: taskmasterThemePlayer,
+    foxIntro: foxIntroPlayer,
+    shaw: shawPlayer,
   };
 
   function playSound(sound: string) {
@@ -38,6 +44,18 @@ export default function Index() {
         onPress={() => playSound("taskmasterTheme")}
       >
         <Text style={styles.soundText}>Taskmaster Theme</Text>
+      </Pressable>
+      <Pressable
+        style={styles.soundButton}
+        onPress={() => playSound("foxIntro")}
+      >
+        <Text style={styles.soundText}>Fox Intro</Text>
+      </Pressable>
+      <Pressable
+        style={styles.soundButton}
+        onPress={() => playSound("shaw")}
+      >
+        <Text style={styles.soundText}>Shaw!</Text>
       </Pressable>
     </View>
   );
