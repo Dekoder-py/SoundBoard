333318655e4b44857cc48c52f33c2150f3ef12f1
Dekoder-py <dekod3r.py@gmail.com>
2025-11-02 14:01:18 +1300

    Initial Commit

---
+++ SoundBoard/SoundBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundBoardApp.swift
+//  SoundBoard
+//
+//  Created by Kyle B on 02/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SoundBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
