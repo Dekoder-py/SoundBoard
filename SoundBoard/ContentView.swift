6ee4cf87a541753149a479dd64b86422fb8ecdca
Dekoder-py <dekod3r.py@gmail.com>
2025-11-02 14:21:22 +1300

    project

--- SoundBoard/ContentView.swift
+++ SoundBoard/ContentView.swift
@@ -6,14 +6,59 @@
 //
 
 import SwiftUI
+import AVFoundation
+
+var audioPlayer: AVAudioPlayer?
+
+func playSound(sound: String) {
+    do {
+        try AVAudioSession.sharedInstance().setCategory(.playback, options: [])
+        try AVAudioSession.sharedInstance().setActive(true)
+    } catch {
+        print("Failed to set audio session category: \(error)")
+        return
+    }
+
+    guard let soundURL = Bundle.main.url(forResource: sound, withExtension: "mp3") else {
+        print("Sound file not found")
+        return
+    }
+
+    do {
+        audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+        audioPlayer?.prepareToPlay()
+        audioPlayer?.play()
+    } catch {
+        print("Failed to initialize AVAudioPlayer: \(error)")
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Grid {
+            Button {
+                playSound(sound: "boom")
+            } label: {
+                Image("rock")
+            }
+            
+            Button {
+                playSound(sound: "shaw")
+            } label: {
+                Image("hornet")
+            }
+            
+            Button {
+                playSound(sound: "tm_theme")
+            } label: {
+                Image("tm_logo")
+            }
+            
+            Button {
+                playSound(sound: "foxTheme")
+            } label: {
+                Image("fox")
+            }
         }
         .padding()
     }
